69842dcacb14760bf5730d9df9129a5d170e9cca
Garvin <garvin.ling@student.csulb.edu>
2013-11-14 01:55:45 -0800

    Landing Page Edit
    Added the info sections back into the landing page layout.

--- public_html/application/views/landing_view.php
+++ public_html/application/views/landing_view.php
@@ -9,32 +9,42 @@
 
 
 <body>
-		
-  	<div style="margin-top:100px; margin-left:10%;">
-   	  
-		  <h1 style="margin-top:0px; color:#EEEEE; font-family: 'Source Sans Pro', sans-serif; font-size:65px; font-weight:200; display:none;" id="hero_title" >Security made simple.</h1>
-		  <br><br>
-		  <p style=" width: 45%; font-family:'Source Sans Pro',sans-serif; color:black; display:none;" id="product_desc">
-		  	We're working on the simplest way to help you keep an eye on things at home.  
-		  </p>
-		  <p style="color:black; font-family:'Source Sans Pro',sans-serif; display:none;" id="hero_paragraph">Sign up below and you'll be the first to know when Scure becomes available!</p>
-		  </p>
-		<br>
-		  
-		  <?php echo form_open('reserve');?>	
-		  <input class ="span2" style="font-family:'Source Sans Pro',sans-serif; font-size:18px; height:29px; width:320px; display:none;" placeholder="Enter E-mail" id="reserve_email" name="reserve_email"/>
-		  <button class="btn btn-danger" style="display:none;"type="submit" id="reserve_submit">Reserve</button>
-		  <?php echo form_close(); ?>
+<!--
+<section data-type="background" style="margin-top:0px; background-color:rgba(255,255,255,0.9); width:100%; background-attachment:fixed;background-size:cover;">
+	
+
+	<section style="background-color:rgba(255,255,255,0.7); height:50px; width:100%; ">
+		 <div style=" margin-left:10%; padding-top:90px;">
+
 </div>
+	
 
 
-<section data-type="background" style="margin-top:100px; background-image:url('../../img/bg5.jpg'); width:100%; background-attachment:fixed;background-size:cover;">
+	</section>
+
+</section>-->
+
+
+<section data-type="background" style="margin-top:0px; background-image:url('../../img/bg5.jpg'); width:100%; background-attachment:fixed;background-size:cover;">
 	
 
 	<section style="background-color:rgba(0,0,0,0.7); height:600px; width:100%; padding-top:30px;">
-		<div id="section1_container" style="margin-left:10%;">
-		  <h1 style="color:white; font-family: 'Source Sans Pro', sans-serif; font-size:65px; font-weight:200;"  >Integrates Seamlessly</h1>
-		 </div>
+		 <div style=" margin-left:10%; padding-top:90px;">
+		  
+		  <h1 style="margin-top:0px; color:#FFFFFF; font-family: 'Source Sans Pro', sans-serif; font-size:65px; font-weight:200; display:none;" id="hero_title" >Security made simple.</h1>
+		  <br><br>
+		  <p style=" color:white;width: 45%; font-family:'Source Sans Pro',sans-serif;  display:none;" id="product_desc">
+		  	We're working on the simplest way to help you keep an eye on things at home.  
+		  </p>
+		  <p style="color:white; font-family:'Source Sans Pro',sans-serif; display:none;" id="hero_paragraph">Sign up below and you'll be the first to know when Scure becomes available!</p>
+		  </p>
+		  <br>
+		  
+		  <?php echo form_open('reserve');?>	
+		  <input class ="span2" style="font-family:'Source Sans Pro',sans-serif; font-size:18px; height:29px; width:320px; display:none;" placeholder="Enter E-mail" id="reserve_email" name="reserve_email"/>
+		  <button class="btn btn-danger" style="display:none;"type="submit" id="reserve_submit" style="margin-left:10px;">Reserve</button>
+		  <?php echo form_close(); ?>
+</div>
 	
 
 
@@ -42,6 +52,60 @@
 
 </section>
 
+
+		
+ 
+
+
+<section data-type="background" style="background-color:white; width:100%; background-attachment:fixed;background-size:cover;">
+	
+
+	<section style="background-color:white; height:600px; width:100%; padding-top:50px;">
+		
+
+		  <div  style="margin-left:10%;">
+		  		<h1 style="color:black; font-family: 'Source Sans Pro', sans-serif; font-size:65px; font-weight:200;">Live Alerts and Updates</h1>
+	
+				 <p style="margin-top:75px;">
+				  Stay up to date with alerts to your smartphone and computer. 
+				  <br>
+				  Investigate by taking a snapshot or pinging one of your friends in the area. 
+
+				</p>
+		 </div>
+	  
+
+
+
+
+
+	
+
+
+	</section>
+
+</section>
+
+<section data-type="background" style="background-image:url('../../img/bg9.jpg'); width:100%; background-attachment:fixed;background-size:cover;">
+	
+
+	<section style="background-color:rgba(0,0,0,0.7); height:600px; width:100%; padding-top:50px; font-family: 'Source Sans Pro', sans-serif;">
+		<div id="section1_container" style="margin-left:10%;">
+		  <h1 style="color:white; font-family: 'Source Sans Pro', sans-serif; font-size:65px; font-weight:200;">Always Connected</h1>
+		 
+		  <p>
+		  Stay 
+
+		  </p>
+
+
+		 </div>
+	
+
+
+	</section>
+
+</section>
 
 
 
