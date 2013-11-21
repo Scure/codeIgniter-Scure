2c7e895923405f9090525821520dc6c4d65c0585
Garvin <garvin.ling@student.csulb.edu>
2013-11-21 01:56:03 -0800

    Blog content update
    Blog content update

--- public_html/application/views/blog_view.php
+++ public_html/application/views/blog_view.php
@@ -11,7 +11,29 @@
 			<h1 style="margin-top:30px;">Read Our Blog.</h1>
 			
 			<br><br><br>
-			
+
+
+			<h3>It's been awhile.   Oh, and hardware! </h3> 
+			<code>11/20/13</code>
+			<p style="width:750px; font-family: 'Lato', sans-serif; color:gray; font-size:16px; line-height:21px;">
+
+			It's been awhile since we've updated our blog but we're working hard on mobile and web development at the moment.  
+			A lot of the team has been working late into the night to achieve this.  One of us is working full-time, the other is at school full-time, 
+			and the other is going through a round of interviews and interning at the moment.  Our goal is to have a working web/mobile prototype by the 
+			mid/end of December.  
+
+			As for the hardware......
+			<br><br>
+			Our little device is now up-and-running its own mini-server.  We're able to communicate with it via our web application.  Of course all of the
+			communication is still extremely basic.  While we're waiting on more parts for our hardware prototype, we're focusing our efforts full-steam to
+			finish as much of the web/mobile application as possible.
+
+			<div class="progress progress-striped active" style="width:800px;">
+			 <div class="bar" style="width: 9%;"></div>
+			</div>
+			</p>
+
+			<br><br><br>
 			
 
 			<h3>Phase 1: Parts and Web Programming</h3> 
